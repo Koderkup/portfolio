d38877e82976d4898ff4903262ba1f986eb2960f
Koderkup <koderkup@gmail.com>
2024-10-17 13:32:31 +0300

    remove single quote

--- src/app/(sub pages)/contact/page.js
+++ src/app/(sub pages)/contact/page.js
@@ -20,14 +20,13 @@ export default function Contact() {
       <article className="relative w-full flex flex-col items-center justify-center py-8 sm:py-0 space-y-8">
         <div className="flex flex-col items-center justify-center space-y-6 w-full sm:w-3/4">
           <h1 className="text-accent font-semibold text-center text-4xl capitalize">
-            Let's cooperate
+            Let’s cooperate
           </h1>
           <p className="text-center font-light text-sm xs:text-base">
-            I’m always open to new opportunities and
-            collaborations! If you’re interested in working together or have any
-            questions, please feel free to reach out. Use the form below to send
-            your message, and I’ll get back to you as soon as possible. Looking
-            forward to connecting!
+            I’m always open to new opportunities and collaborations! If you’re
+            interested in working together or have any questions, please feel
+            free to reach out. Use the form below to send your message, and I’ll
+            get back to you as soon as possible. Looking forward to connecting!
           </p>
         </div>
         <Form />
